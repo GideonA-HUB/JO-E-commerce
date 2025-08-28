c83d17c4be41f6c4df0d134bc2759a5a10e7df5a
Gideon Amienghemhen <amienz0958@gmail.com>
2025-08-28 23:27:00 +0100

    Remove unused stripe import

--- backend/api/models.py
+++ backend/api/models.py
@@ -4,7 +4,6 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.db.models import Avg, Count
-import stripe
 
 from django.contrib.auth import get_user_model
 User = get_user_model()
